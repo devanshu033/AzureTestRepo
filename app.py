546afa359e498356cfda34174490272635ffe965
Jagat Jyoti Behera <bjagatjyoti@TOSHIBA-TSIP.COM>
2024-04-16 00:29:28 +0530

    file need changes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import pytesseract
+import cv2
 
 app = Flask(__name__)
 
 @app.route('/test')
 def hello_world():
-  return "Hello, World!68"
+  return "Hello, World!6844"
 
 if __name__ == '__main__':
   app.run(debug=True)
